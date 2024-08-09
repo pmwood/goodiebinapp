65ba0506cdeeab359618d8f895c357e1d8af1dd7
pmwood <pmwood@gmail.com>
2024-08-09 02:05:59 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,11 @@ import os
 
 AZURE_STORAGE_CONNECTION_STRING = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 AZURE_CONTAINER_NAME = os.getenv('AZURE_CONTAINER_NAME')
+
+# Entra ID configuration
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+AUTHORITY = os.getenv('AUTHORITY')  # e.g., 'https://login.microsoftonline.com/your_tenant_id'
+REDIRECT_PATH = os.getenv('REDIRECT_PATH')  # e.g., '/getAToken'
+SCOPE = ["User.Read"]
+SESSION_TYPE = "filesystem"
